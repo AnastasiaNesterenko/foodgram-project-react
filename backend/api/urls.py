a791702309094abbd33c43ec0c270e83c2ff3760
Анастасия Нестеренко <nesterenko.anastasia1996@gmail.com>
2022-07-14 20:23:29 +0700

    'попытка_5'

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 
-from rest_framework.routers import DefaultRouter
-
 from .views import (DownloadShoppingCart,
                     IngredientViewSet,
                     RecipeViewSet, TagViewSet)
+
+from rest_framework.routers import DefaultRouter
+
 
 app_name = 'api'
 
