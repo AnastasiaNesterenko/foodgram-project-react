497aaef5bd46f45c2ba88af0fe064ededb084be7
Анастасия Нестеренко <nesterenko.anastasia1996@gmail.com>
2022-08-06 14:25:45 +0700

    'ненавижу_флаке8'

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,7 +6,7 @@ from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfgen import canvas
 
 from rest_framework import viewsets, views, status
-from rest_framework.decorators import action
+# from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
