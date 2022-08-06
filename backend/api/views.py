4db8ea58046ce1c0cdadde4add4b02e91dfb45c9
Анастасия Нестеренко <nesterenko.anastasia1996@gmail.com>
2022-08-06 14:37:55 +0700

    'ненавижу_флаке8'

--- backend/api/views.py
+++ backend/api/views.py
@@ -9,7 +9,6 @@ from rest_framework import viewsets, views, status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-# from rest_framework.views import APIView
 
 from .filters import RecipeFilter, IngredientSearchFilter
 from .pagination import CustomPageNumberPagination
