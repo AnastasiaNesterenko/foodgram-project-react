07f80dd74bcb2412b9e81cc2d0f0f69cee890e73
Анастасия Нестеренко <nesterenko.anastasia1996@gmail.com>
2022-08-07 16:05:15 +0700

    'переписала_вьюсеты_для_избраного_и_корзины'

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -4,8 +4,11 @@ from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 
-from recipes.models import (FavoriteRecipe, Ingredient, Recipe,
-                            IngredientAmount, Cart, Tag)
+# from recipes.models import (FavoriteRecipe, Ingredient, Recipe,
+#                             IngredientAmount, Cart, Tag)
+from recipes.models import (Ingredient, Recipe,
+                            IngredientAmount, Tag)
+from users.models import Follow
 from users.serializers import CustomUserSerializer
 
 
@@ -159,42 +162,74 @@ class ShortRecipeSerializer(serializers.ModelSerializer):
         read_only_fields = ('id', 'name', 'image', 'cooking_time')
 
 
-class FavoriteRecipeSerializer(serializers.ModelSerializer):
+# class FavoriteRecipeSerializer(serializers.ModelSerializer):
+#
+#     class Meta:
+#         fields = ('user', 'recipe')
+#         model = FavoriteRecipe
+#         validators = [
+#             UniqueTogetherValidator(
+#                 queryset=FavoriteRecipe.objects.all(),
+#                 fields=('user', 'recipe'),
+#                 message='Рецепт уже добавлен в избранное'
+#             )
+#         ]
+#
+#     def to_representation(self, instance):
+#         request = self.context.get('request')
+#         return ShortRecipeSerializer(
+#             instance.recipe,
+#             context={'request': request}
+#         ).data
+#
+#
+# class CartSerializer(FavoriteRecipeSerializer):
+#     class Meta(FavoriteRecipeSerializer.Meta):
+#         model = Cart
+#         fields = ('id', 'user', 'recipe')
+#         validators = [
+#             UniqueTogetherValidator(
+#                 queryset=Cart.objects.all(),
+#                 fields=('user', 'recipe'),
+#                 message='Рецепт уже добавлен в список покупок'
+#             )
+#         ]
+#
+#     def to_representation(self, instance):
+#         request = self.context.get('request')
+#         return ShortRecipeSerializer(
+#             instance.recipe,
+#             context={'request': request}
+#         ).data
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField(source='author.id')
+    email = serializers.ReadOnlyField(source='author.email')
+    username = serializers.ReadOnlyField(source='author.username')
+    first_name = serializers.ReadOnlyField(source='author.first_name')
+    last_name = serializers.ReadOnlyField(source='author.last_name')
+    is_subscribed = serializers.SerializerMethodField()
+    recipes = serializers.SerializerMethodField()
+    recipes_count = serializers.SerializerMethodField()
 
     class Meta:
-        fields = ('user', 'recipe')
-        model = FavoriteRecipe
-        validators = [
-            UniqueTogetherValidator(
-                queryset=FavoriteRecipe.objects.all(),
-                fields=('user', 'recipe'),
-                message='Рецепт уже добавлен в избранное'
-            )
-        ]
+        model = Follow
+        fields = ('id', 'email', 'username', 'first_name', 'last_name',
+                  'is_subscribed', 'recipes', 'recipes_count')
 
-    def to_representation(self, instance):
+    def get_is_subscribed(self, obj):
+        return Follow.objects.filter(
+            user=obj.user, author=obj.author
+        ).exists()
+
+    def get_recipes(self, obj):
         request = self.context.get('request')
-        return ShortRecipeSerializer(
-            instance.recipe,
-            context={'request': request}
-        ).data
+        limit = request.GET.get('recipes_limit')
+        queryset = Recipe.objects.filter(author=obj.author)
+        if limit:
+            queryset = queryset[:int(limit)]
+        return ShortRecipeSerializer(queryset, many=True).data
 
-
-class CartSerializer(FavoriteRecipeSerializer):
-    class Meta(FavoriteRecipeSerializer.Meta):
-        model = Cart
-        fields = ('id', 'user', 'recipe')
-        validators = [
-            UniqueTogetherValidator(
-                queryset=Cart.objects.all(),
-                fields=('user', 'recipe'),
-                message='Рецепт уже добавлен в список покупок'
-            )
-        ]
-
-    def to_representation(self, instance):
-        request = self.context.get('request')
-        return ShortRecipeSerializer(
-            instance.recipe,
-            context={'request': request}
-        ).data
+    def get_recipes_count(self, obj):
+        return Recipe.objects.filter(author=obj.author).count()
--- backend/api/views.py
+++ backend/api/views.py
@@ -13,9 +13,12 @@ from rest_framework.response import Response
 from .filters import RecipeFilter, IngredientSearchFilter
 from .pagination import CustomPageNumberPagination
 from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
-from .serializers import (CartSerializer, FavoriteRecipeSerializer,
-                          IngredientSerializer, RecipeListSerializer,
-                          RecipeSerializer, TagSerializer)
+# from .serializers import (CartSerializer, FavoriteRecipeSerializer,
+#                           IngredientSerializer, RecipeListSerializer,
+#                           RecipeSerializer, TagSerializer)
+from .serializers import (IngredientSerializer, RecipeListSerializer,
+                          RecipeSerializer, TagSerializer,
+                          ShortRecipeSerializer)
 from recipes.models import (FavoriteRecipe, Ingredient, Recipe,
                             IngredientAmount, Cart, Tag)
 
@@ -27,53 +30,93 @@ class RecipeViewSet(viewsets.ModelViewSet):
     permission_classes = (IsOwnerOrReadOnly,)
     filter_class = RecipeFilter
 
-    def get_serializer_class(self):
-        if self.action in ('list', 'retrieve'):
-            return RecipeListSerializer
-        return RecipeSerializer
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
-    def get_serializer_context(self):
-        context = super().get_serializer_context()
-        context.update({'request': self.request})
-        return context
+    @action(detail=True, methods=['GET', 'DELETE'],
+            permission_classes=[IsAuthenticated])
+    def favorite(self, request, pk=None):
+        if request.method == 'GET':
+            return self.add_obj(FavoriteRecipe, request.user, pk)
+        elif request.method == 'DELETE':
+            return self.delete_obj(FavoriteRecipe, request.user, pk)
+        return None
 
-    @staticmethod
-    def post_method_for_actions(request, pk, serializers):
-        data = {'user': request.user.id, 'recipe': pk}
-        serializer = serializers(data=data, context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+    @action(detail=True, methods=['GET', 'DELETE'],
+            permission_classes=[IsAuthenticated])
+    def shopping_cart(self, request, pk=None):
+        if request.method == 'GET':
+            return self.add_obj(Cart, request.user, pk)
+        elif request.method == 'DELETE':
+            return self.delete_obj(Cart, request.user, pk)
+        return None
+
+    def add_obj(self, model, user, pk):
+        if model.objects.filter(user=user, recipe__id=pk).exists():
+            return Response({
+                'errors': 'Рецепт уже добавлен в список'
+            }, status=status.HTTP_400_BAD_REQUEST)
+        recipe = get_object_or_404(Recipe, id=pk)
+        model.objects.create(user=user, recipe=recipe)
+        serializer = ShortRecipeSerializer(recipe)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    @staticmethod
-    def delete_method_for_actions(request, pk, model):
-        user = request.user
-        recipe = get_object_or_404(Recipe, id=pk)
-        model_obj = get_object_or_404(model, user=user, recipe=recipe)
-        model_obj.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete_obj(self, model, user, pk):
+        obj = model.objects.filter(user=user, recipe__id=pk)
+        if obj.exists():
+            obj.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response({
+            'errors': 'Рецепт уже удален'
+        }, status=status.HTTP_400_BAD_REQUEST)
 
-    @action(detail=True, methods=["POST"],
-            permission_classes=[IsAuthenticated],)
-    def favorite(self, request, pk):
-        return self.post_method_for_actions(
-            request=request, pk=pk, serializers=FavoriteRecipeSerializer)
-
-    @favorite.mapping.delete
-    def delete_favorite(self, request, pk):
-        return self.delete_method_for_actions(
-            request=request, pk=pk, model=FavoriteRecipe)
-
-    @action(detail=True, methods=["POST"],
-            permission_classes=[IsAuthenticated],)
-    def shopping_cart(self, request, pk):
-        return self.post_method_for_actions(
-            request=request, pk=pk, serializers=CartSerializer)
-
-    @shopping_cart.mapping.delete
-    def delete_shopping_cart(self, request, pk):
-        return self.delete_method_for_actions(
-            request=request, pk=pk, model=Cart)
+    # def get_serializer_class(self):
+    #     if self.action in ('list', 'retrieve'):
+    #         return RecipeListSerializer
+    #     return RecipeSerializer
+    #
+    # def get_serializer_context(self):
+    #     context = super().get_serializer_context()
+    #     context.update({'request': self.request})
+    #     return context
+    #
+    # @staticmethod
+    # def post_method_for_actions(request, pk, serializers):
+    #     data = {'user': request.user.id, 'recipe': pk}
+    #     serializer = serializers(data=data, context={'request': request})
+    #     serializer.is_valid(raise_exception=True)
+    #     serializer.save()
+    #     return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #
+    # @staticmethod
+    # def delete_method_for_actions(request, pk, model):
+    #     user = request.user
+    #     recipe = get_object_or_404(Recipe, id=pk)
+    #     model_obj = get_object_or_404(model, user=user, recipe=recipe)
+    #     model_obj.delete()
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
+    #
+    # @action(detail=True, methods=["POST"],
+    #         permission_classes=[IsAuthenticated],)
+    # def favorite(self, request, pk):
+    #     return self.post_method_for_actions(
+    #         request=request, pk=pk, serializers=FavoriteRecipeSerializer)
+    #
+    # @favorite.mapping.delete
+    # def delete_favorite(self, request, pk):
+    #     return self.delete_method_for_actions(
+    #         request=request, pk=pk, model=FavoriteRecipe)
+    #
+    # @action(detail=True, methods=["POST"],
+    #         permission_classes=[IsAuthenticated],)
+    # def shopping_cart(self, request, pk):
+    #     return self.post_method_for_actions(
+    #         request=request, pk=pk, serializers=CartSerializer)
+    #
+    # @shopping_cart.mapping.delete
+    # def delete_shopping_cart(self, request, pk):
+    #     return self.delete_method_for_actions(
+    #         request=request, pk=pk, model=Cart)
 
 
 class TagViewSet(viewsets.ReadOnlyModelViewSet):
