66693b5d50920ea85ee756b9a0aaba8c191f2c94
Анастасия Нестеренко <nesterenko.anastasia1996@gmail.com>
2022-08-08 20:23:06 +0700

    'ну_с_с_богом_2'

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,6 @@
 from django.urls import include, path
 
-from .views import (DownloadShoppingCart,
-                    IngredientViewSet,
+from .views import (IngredientViewSet,
                     RecipeViewSet, TagViewSet)
 
 from rest_framework.routers import DefaultRouter
@@ -16,8 +15,8 @@ router.register('ingredients', IngredientViewSet, basename='ingredients')
 
 
 urlpatterns = [
-    path('recipes/download_shopping_cart/',
-         DownloadShoppingCart.as_view(), name='download'),
+    # path('recipes/download_shopping_cart/',
+    #      DownloadShoppingCart.as_view(), name='download'),
     path('', include(router.urls)),
     # path('recipes/<int:favorite_id>/favorite/', FavoriteApiView.as_view()),
     # path('recipes/<int:recipe_id>/shopping_cart/', ShoppingView.as_view()),
